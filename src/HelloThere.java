d43c1238aa6e5b6029809daed741b9bb967ec892
Javier <d2j5@hotmail.com>
2023-05-09 17:03:08 -0400

    Initial commit

---
+++ src/HelloThere.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HelloThere {
+    public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Hello there! How are y ou doing today");
+
+            String userInput = scanner.nextLine();
+
+            System.out.println("Wow - Interesting. Tell me more about you!");
+
+            String userInput2 = scanner.nextLine();
+
+            System.out.println("It was nice to see you. Good luck!");
+
+    }
+}
